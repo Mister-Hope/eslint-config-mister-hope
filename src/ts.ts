71ad5c9468bb9b8c2e384e5625d167c3141e6c6a
Mister-Hope <mister-hope@outlook.com>
2024-12-09 00:34:53 +0800

    chore: improve ts config

--- src/ts.ts
+++ src/ts.ts
@@ -59,6 +59,7 @@ export const ts = config(
           format: ["PascalCase"],
         },
       ],
+      "@typescript-eslint/no-dynamic-delete": "off",
       "@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
       "@typescript-eslint/no-unsafe-member-access": "warn",
       "@typescript-eslint/prefer-nullish-coalescing": "warn",
