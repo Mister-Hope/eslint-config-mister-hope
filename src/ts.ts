b6b7be65029aade2320af15055c44c0cfaf3825e
Mister-Hope <mister-hope@outlook.com>
2024-12-10 12:50:25 +0800

    chore: tweaks

--- src/ts.ts
+++ src/ts.ts
@@ -73,6 +73,11 @@ export const ts = config(
       "@typescript-eslint/no-invalid-void-type": "off",
       "@typescript-eslint/no-non-null-assertion": "warn",
       "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unnecessary-condition": [
+        "error",
+        { allowConstantLoopConditions: true },
+      ],
+
       "@typescript-eslint/prefer-nullish-coalescing": [
         "warn",
         {
--- src/vitest.ts
+++ src/vitest.ts
@@ -8,9 +8,22 @@ export const vitest = config(
     ...pluginVitest.configs.recommended,
   },
   {
-    files: ["**/*.spec.{j,t}s"],
+    files: ["**/*.spec.ts"],
     rules: {
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/unbound-method": "off",
+    },
+  },
+  {
+    files: ["**/*.spec-d.ts"],
+    rules: {
+      "@typescript-eslint/no-confusing-void-expression": "off",
     },
   },
 );
